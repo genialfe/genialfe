2eaccaea333e4725f83391930aac760c915b3b66
zzy <zhouhoushu@126.com>
2020-12-02 14:46:31 +0800

    add antd

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Button } from 'antd'
+
 import './style.css'
 
 export default function App() {
   return (
     <div className="App">
-      Genial Trial demo
-      
+      <h2>Genial Trial demo</h2>
+    
+      <Button>TEST ANTD BUTTON</Button>
     </div>
   );
 }
