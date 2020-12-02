ec997ec02785dae65c99862e7a58ae310260f7ab
zzy <zhouhoushu@126.com>
2020-12-02 14:32:43 +0800

    reinitialize

---
+++ src/components/Login/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export interface ILoginProps {
+
+}
+
+export default class Login extends React.Component <ILoginProps, any> {
+
+  render() {
+    return (
+      <div>
+        DEMO
+      </div>
+    )
+  }
+
+}
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export interface INavBarProps {
+
+}
+
+export default class NavBar extends React.Component <INavBarProps, any> {
+
+  render() {
+    return (
+      <div>
+        DEMO
+      </div>
+    )
+  }
+
+}
