62c27d3952e73fe48f96f30b54f6833c1cfc5dee
liuwenyao <Liuwenyao2017@126.com>
2020-12-22 16:07:52 +0800

    同步App index

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,15 +1,60 @@
 import React from 'react'
-import { Button } from 'antd'
+import { action, observable } from 'mobx'
+import { observer } from 'mobx-react'
+import { Button, Switch } from 'antd'
+import bind from '../../utils/bind'
 
 import './style.css'
+import Profile from '../Profile'
 
-export default function App() {
-  return (
-    <div className="App">
-      <h2>Genial Trial demo</h2>
-    
-      <Button>TEST ANTD BUTTON</Button>
-    </div>
-  );
+export interface IAppProps {
+
 }
 
+@observer
+export default class App extends React.Component<IAppProps, any> {
+
+  @observable isTestMode: boolean = false
+
+  @bind
+  @action
+  setIsTestMode(value: boolean) {
+    this.isTestMode = value
+  }
+
+  @bind
+  handleSwitchChange(checked: boolean) {
+    this.setIsTestMode(checked)
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <div className='TestModeSwitch'>
+          测试模式 {this.isTestMode ? 'true' : 'false'}
+          <Switch 
+            checkedChildren='开启'
+            unCheckedChildren='关闭'
+            onChange={this.handleSwitchChange}
+          />
+        </div>
+        {
+          <Profile 
+            name='马斯克' 
+            location='上海' 
+            introduction='测试' 
+            interests={['天使投资', '数据挖掘']}
+            connections={0}
+          />
+        }
+        {
+          this.isTestMode && (
+            <div>
+              <Button>TEST ANTD BUTTON</Button>
+            </div>
+          )
+        }
+      </div>
+    )
+  }
+}
