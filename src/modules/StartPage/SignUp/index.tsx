25dcf750404c78f8192ac09cd424fb0f0d6c8ca5
zzy <zhouhoushu@126.com>
2021-02-08 22:36:59 +0800

    fix: 修复199开头手机号无法注册的问题

--- src/modules/StartPage/SignUp/index.tsx
+++ src/modules/StartPage/SignUp/index.tsx
@@ -22,7 +22,7 @@ export default class SignUp extends React.Component<ISignUpProps, any> {
   }
 
   isPhoneNumber(number: string) {
-    const reg = /^0?1[3|4|5|6|7|8][0-9]\d{8}$/
+    const reg = /^0?1[3|4|5|6|7|8|9][0-9]\d{8}$/
     return reg.test(number)
   }
 
--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,4 +1,4 @@
 export function isPhoneNumber(number: string) {
-  const reg = /^0?1[3|4|5|6|7|8][0-9]\d{8}$/
+  const reg = /^0?1[3|4|5|6|7|8|9][0-9]\d{8}$/
   return reg.test(number)
 }
